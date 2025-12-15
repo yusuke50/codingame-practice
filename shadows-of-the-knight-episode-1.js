fda16b8192e83b8807a6fccd7da7315704e1559a
lin <linfanchiang@ieiworld.com>
2025-12-15 13:39:03 +0800

    Refactor with boundaries

--- shadows-of-the-knight-episode-1.js
+++ shadows-of-the-knight-episode-1.js
@@ -1,51 +1,43 @@
 /* Shadows Of The Knight - Episode 1
    https://www.codingame.com/training/medium/shadows-of-the-knight-episode-1
-*/ 
+*/
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+// v2 with AI
 
 var inputs = readline().split(' ');
-const W = parseInt(inputs[0]); // width of the building.
-const H = parseInt(inputs[1]); // height of the building.
-const N = parseInt(readline()); // maximum number of turns before game over.
+const W = parseInt(inputs[0]);
+const H = parseInt(inputs[1]);
+const N = parseInt(readline());
 var inputs = readline().split(' ');
 const X0 = parseInt(inputs[0]);
 const Y0 = parseInt(inputs[1]);
+
 let curX = X0;
 let curY = Y0;
-let lastDistanceX = Math.floor(W / 2);
-let lastDistanceY = Math.floor(H / 2);
 
-// game loop
+// Track the search boundaries
+let minX = 0, maxX = W - 1;
+let minY = 0, maxY = H - 1;
+
 while (true) {
-    const bombDir = readline(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
-    // Write an action using console.log()
-    // To debug: console.error('Debug messages...');
+    const bombDir = readline();
 
-    let tempX = curX;
-    let tempY = curY;
-
-    if (bombDir.indexOf('L') > -1) {
-        tempX -= lastDistanceX;
-    } else if (bombDir.indexOf('R') > -1) {
-        tempX += lastDistanceX;
+    // Update boundaries based on direction
+    if (bombDir.includes('L')) {
+        maxX = curX - 1;
+    } else if (bombDir.includes('R')) {
+        minX = curX + 1;
     }
 
-    if (bombDir.indexOf('U') > -1) {
-        tempY -= lastDistanceY;
-    } else if (bombDir.indexOf('D') > -1) {
-        tempY += lastDistanceY;
+    if (bombDir.includes('U')) {
+        maxY = curY - 1;
+    } else if (bombDir.includes('D')) {
+        minY = curY + 1;
     }
 
-    curX = Math.min(Math.max(tempX, 0), W - 1)
-    curY = Math.min(Math.max(tempY, 0), H - 1)
+    // Jump to the middle of the remaining search space
+    curX = Math.floor((minX + maxX) / 2);
+    curY = Math.floor((minY + maxY) / 2);
 
-    // the location of the next window Batman should jump to.
     console.log(curX, curY);
-
-    lastDistanceX = Math.round((lastDistanceX / 2))
-    lastDistanceY = Math.round((lastDistanceY / 2))
 }
