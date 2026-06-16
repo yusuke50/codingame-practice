7b6f4b490de94211995dd5475fc88e9a41238ee2
Lin Fan-Chiang <yusuke050@gmail.com>
2026-06-16 17:48:43 +0800

    Update the-travelling-salesman-problem.js

--- the-travelling-salesman-problem.js
+++ the-travelling-salesman-problem.js
@@ -2,67 +2,44 @@
    https://www.codingame.com/training/easy/the-travelling-salesman-problem
 */
 
-// v1
-
+// v2 with AI
 const N = parseInt(readline());
-const citiesMap = new Map();
+const cities = [];
 for (let i = 0; i < N; i++) {
     var inputs = readline().split(' ');
     const X = parseInt(inputs[0]);
     const Y = parseInt(inputs[1]);
-    citiesMap.set(i, { x: X, y: Y, visited: false });
+    cities.push({ x: X, y: Y, visited: false });
 }
-const firstCity = citiesMap.get(0);
 
-citiesMap.set(0, { ...firstCity, visited: true });
-let currX = firstCity.x;
-let currY = firstCity.y;
+let currIdx = 0;
+cities[currIdx].visited = true;
 let total = 0;
-let calcResult = [];
-let lastCity;
 
-const calcDist = () => {
-    citiesMap.forEach((item, idx) => {
-        if (!item.visited) {
-            let diffX = item.x - currX;
-            let diffY = item.y - currY;
-            let dist = Math.sqrt(diffX * diffX + diffY * diffY);
-            calcResult.push({ idx, dist });
-        }
-    });
-
-    if (calcResult.length > 0) {
-        let minObj = calcResult[0];
-        let minDist = Infinity;
-        calcResult.forEach((obj) => {
-            if (obj.dist < minDist) {
-                minObj = obj;
-                minDist = obj.dist;
-            }
-        });
-
-        total += minDist;
-        let newCity = citiesMap.get(minObj.idx);
-        citiesMap.set(minObj.idx, { ...newCity, visited: true });
-        currX = newCity.x;
-        currY = newCity.y;
-
-        if (calcResult.length == 1) {
-            lastCity = citiesMap.get(minObj.idx);
-        }
-        calcResult = [];
-        return 1;
-    } else {
-        return 'empty';
-    }
-};
-
-for (let i = 0; i < N; i += 1) {
-    let res = calcDist();
-    if (res == 'empty') {
-        let lastDistX = lastCity.x - firstCity.x;
-        let lastDistY = lastCity.y - firstCity.y;
-        let lastDist = Math.sqrt(lastDistX * lastDistX + lastDistY * lastDistY);
-        console.log(Math.round(total + lastDist));
-    }
+const getDistance = (c1, c2) => {
+    const diffX = c1.x - c2.x;
+    const diffY = c1.y - c2.y;
+    return Math.sqrt(diffX * diffX + diffY * diffY);
 }
+
+for (let step = 0; step < N - 1; step += 1) {
+    let minIdx = -1;
+    let minDist = Infinity;
+
+    for (let i = 0; i < N; i += 1) {
+        if (!cities[i].visited) {
+            const dist = getDistance(cities[currIdx], cities[i]);
+            if (dist < minDist) {
+                minDist = dist;
+                minIdx = i;
+            }
+        }
+    }
+
+    total += minDist;
+    cities[minIdx].visited = true;
+    currIdx = minIdx;
+}
+
+total += getDistance(cities[currIdx], cities[0]);
+console.log(Math.round(total));
